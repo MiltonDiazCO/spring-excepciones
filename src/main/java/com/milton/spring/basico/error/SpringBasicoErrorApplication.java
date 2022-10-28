85a8086b064534508c78ff93f474eedb88a0018c
MiltonDiazCO <diazmartinezmilton@gmail.com>
2022-10-27 20:40:08 -0500

    Initial commit

---
+++ src/main/java/com/milton/spring/basico/error/SpringBasicoErrorApplication.java
@@ -0,0 +1,13 @@
+package com.milton.spring.basico.error;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBasicoErrorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBasicoErrorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/milton/spring/basico/error/SpringBasicoErrorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.milton.spring.basico.error;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBasicoErrorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
